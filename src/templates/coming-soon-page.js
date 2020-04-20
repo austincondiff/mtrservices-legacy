0e96be065cda0cad2b62035e342acb6412e49ee3
Austin Condiff <austin.condiff@gmail.com>
2020-04-20 16:52:46 -0500

    Added button hover states, converted to styled components, and enabled notify button

--- src/templates/coming-soon-page.js
+++ src/templates/coming-soon-page.js
@@ -1,145 +1,141 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { Link, graphql } from 'gatsby'
+import styled from 'styled-components'
 
 import Layout from '../components/Layout'
 import Features from '../components/Features'
 import BlogRoll from '../components/BlogRoll'
 
-export const IndexPageTemplate = ({
-  image,
-  title,
-  heading,
-  subheading,
-  mainpitch,
-  description,
-  intro,
-}) => (
-  <div>
-    <div
-      className="full-width-image margin-top-0"
-      style={{
-        backgroundColor: '#090D1C',
-        backgroundImage: `url(${
-          !!image.childImageSharp ? image.childImageSharp.fluid.src : image
-        })`,
-        backgroundPosition: `top left`,
-        backgroundAttachment: `fixed`,
-        position: 'relative',
-        height: '100vh'
-      }}
-      >
-      <div style={{
-        position: 'absolute',
-        top: 0,
-        right: 0,
-        bottom: 0,
-        left: 0,
-        background: 'linear-gradient(90deg, rgba(9,13,28,1) 0%, rgba(9,13,28,0.5) 100%)'
-      }}></div>
-      <div style={{
-        position: 'absolute',
-        top: 0,
-        right: '-50%',
-        bottom: 0,
-        left: '75%',
-        transform: 'skew(-15deg, 0deg)',
-        background: 'linear-gradient(0deg, #F25E1C 0%, #EDC621 100%)'
-      }}></div>
-      <div
-        className="container"
-        style={{
-          display: 'flex',
-          height: '150px',
-          lineHeight: '1',
-          justifyContent: 'space-around',
-          alignItems: 'left',
-          flexDirection: 'column',
-          position: 'relative'
-        }}
-        >
-        <h1
-          className="has-text-weight-bold is-size-3-mobile is-size-2-tablet is-size-1-widescreen"
-          style={{
-            color: 'white',
-            fontFamily: 'museo-sans, sans-serif',
-            fontWeight: '700',
-            fontSize: '56px',
-            letterSpacing: 0,
-            lineHeight: '64px',
-            width: '25%',
-            marginBottom: '32px'
-          }}
-          >
-          {/*title*/}
-          Our new website is on it’s way.
-        </h1>
-        <h3
-          className="has-text-weight-bold is-size-5-mobile is-size-5-tablet is-size-4-widescreen"
-          style={{
-            fontFamily: 'museo-sans, sans-serif',
-            fontWeight: '700',
-            fontSize: '20px',
-            color: '#F7B154',
-            lineHeight: '32px',
-            width: '50%',
-            marginBottom: '64px'
-          }}
-          >
-          {/*subheading*/}
-          Need IT help? Get support now or sign up to be notified when we launch our new website.
-        </h3>
-        <div>
-          <button onClick={() => window.open('http://support.mtrservices.com/download_client_connector?id=2&name=Mark+Condiff')} style={{
-            backgroundImage: `linear-gradient(134deg, #F25E1C 0%, #EDC621 100%)`,
-            borderRadius: 4,
-            color: 'white',
-            fontFamily: 'museo-sans, sans-serif',
-            fontWeight: '700',
-            fontSize: '16px',
-            letterSpacing: '0.57px',
-            textAlign: 'center',
-            width: 240,
-            lineHeight: '56px',
-            border: 0,
-            marginRight: 16
-          }}>Get support</button>
-          <button style={{
-            background: 'transparent',
-            // backgroundImage: `linear-gradient(134deg, #F25E1C 0%, #EDC621 100%)`,
-            borderRadius: 4,
-            color: 'white',
-            fontFamily: 'museo-sans, sans-serif',
-            fontWeight: '700',
-            fontSize: '16px',
-            letterSpacing: '0.57px',
-            textAlign: 'center',
-            width: 240,
-            lineHeight: '56px',
-            border: 0,
-            position: 'relative'
-          }}>
-          <svg style={{
-              position: 'absolute',
-              top: 0,
-              left: 0,
-              width: '100%',
-              height: '100%'
-            }} width="100%" height="100%">
-            <defs>
-              <linearGradient x1="0%" y1="0%" x2="100%" y2="100%" id="linearGradient">
-                <stop stopColor="#F25E1C" offset="0%"></stop>
-                <stop stopColor="#EDC621" offset="100%"></stop>
-              </linearGradient>
-            </defs>
-            <rect id="Rectangle-Copy" fill="none" stroke="url(#linearGradient)" strokeWidth="3" x="1.5" y="1.5" width="calc(100% - 3px)" height="calc(100% - 3px)" rx="4"></rect>
-          </svg>
-          <span>Be notified</span>
-        </button>
-        </div>
+const Wrap = styled.div`
+  background-color: #090D1C;
+  background-image: url(${({imgUrl}) => imgUrl});
+  background-position: top left;
+  background-attachment: fixed;
+  position: relative;
+  height: 100vh;
+  width: 100vw;
+  background-size: cover;
+  background-position: bottom;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+const Cover = styled.div`
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  background: linear-gradient(90deg, rgba(9,13,28,1) 0%, rgba(9,13,28,0.5) 100%);
+`;
+const AngledAccent = styled.div`
+  position: absolute;
+  top: 0;
+  right: -50%;
+  bottom: 0;
+  left: 75%;
+  transform: skew(-15deg, 0deg);
+  background: linear-gradient(0deg, #F25E1C 0%, #EDC621 100%);
+`
+const Content = styled.div`
+  display: flex;
+  height: 150px;
+  lineHeight: 1;
+  justify-content: space-around;
+  align-items: left;
+  flex-direction: column;
+  position: relative;
+`
+const Title = styled.h1`
+  color: white;
+  font-family: museo-sans, sans-serif;
+  font-weight: 700;
+  font-size: 48px;
+  letter-spacing: 0;
+  line-height: 64px;
+  width: 25%;
+  margin-bottom: 32px;
+`
+const Subtitle = styled.h3`
+  color: #F7B154;
+  font-family: museo-sans, sans-serif;
+  font-weight: 700;
+  font-size: 24px;
+  line-height: 32px;
+  width: 50%;
+  margin-bottom: 64px;
+`
+const ButtonWrap = styled.button`
+  background: ${({primary}) => primary ? `linear-gradient(134deg, #F25E1C 0%, #EDC621 100%)` : `transparent`};
+  border-radius: 4px;
+  color: white;
+  font-family: museo-sans, sans-serif;
+  font-weight: 700;
+  font-size: 16px;
+  letter-spacing: 0.57px;
+  text-align: center;
+  width: 240px;
+  line-height: 56px;
+  border: 0;
+  margin-right: 16px;
+  position: relative;
+  cursor: pointer;
+  transition: 200ms;
+  outline: none;
+  transform: scale(1);
+  box-shadow: 0 0 0 rgba(242,94,28,0);
+  &:hover {
+    transform: scale(1.05);
+    box-shadow: 0 16px 48px rgba(242,94,28,0.25);
+    ${({primary}) => primary ? `` : `background-color: rgba(242,94,28,0.1);`}
+  }
+`
+
+const Button = ({ children, primary, ...props }) => (
+  <ButtonWrap primary={primary} {...props}>
+    {!primary && (
+      <svg style={{
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          width: '100%',
+          height: '100%'
+        }} width="100%" height="100%">
+        <defs>
+          <linearGradient x1="0%" y1="0%" x2="100%" y2="100%" id="linearGradient">
+            <stop stopColor="#F25E1C" offset="0%"></stop>
+            <stop stopColor="#EDC621" offset="100%"></stop>
+          </linearGradient>
+        </defs>
+        <rect id="Rectangle-Copy" fill="none" stroke="url(#linearGradient)" strokeWidth="3" x="1.5" y="1.5" width="calc(100% - 3px)" height="calc(100% - 3px)" rx="4"></rect>
+      </svg>
+    )}
+    <span>{children}</span>
+  </ButtonWrap>
+)
+
+export const IndexPageTemplate = ({ image }) => (
+  <Wrap imgUrl={!!image.childImageSharp ? image.childImageSharp.fluid.src : image}>
+    <Cover />
+    <AngledAccent />
+    <Content className="container">
+      <Title>
+        {/*title*/}
+        Our new website is on it’s way.
+      </Title>
+      <Subtitle>
+        {/*subheading*/}
+        Need IT help? Get support now or sign up to be notified when we launch our new website.
+      </Subtitle>
+      <div>
+        <Button primary onClick={() => window.open('http://support.mtrservices.com/download_client_connector?id=2&name=Mark+Condiff')}>Get support</Button>
+        <Button onClick={() => window.open('http://eepurl.com/g0xVWH')}>
+          Be notified
+        </Button>
       </div>
-    </div>
-  </div>
+    </Content>
+  </Wrap>
 )
 
 IndexPageTemplate.propTypes = {
